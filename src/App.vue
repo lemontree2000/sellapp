9d66c0f2f09671cf3452367356cc8441490625cb
lemontree <gonelove.guo@gmail.com>
2017-02-12 19:10:43 +0800

    git commit -m "add tab"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div class="tab">
+      <ul>
+        <li class="tab-item">
+          <router-link to="/goods">商品</router-link>
+        </li>
+        <li class="tab-item">
+          <router-link to="/goods">评价</router-link>
+        </li>
+        <li class="tab-item">
+          <router-link to="/goods">店铺</router-link>
+        </li>
+      </ul>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -11,7 +24,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" rel="stylesheet/less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +32,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .tab {
+    & ul {
+      padding: 0;
+      margin: 0;
+      display: flex;
+      & .tab-item {
+        list-style: none;
+        flex: 1;
+        & > a {
+          display: block;
+          text-decoration: none;
+          color: #333;
+        }
+      }
+    }
+  }
 }
 </style>
