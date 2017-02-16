be962b5b9b25c45f0ebe8fb68cf67d435c148f82
lemontree <gonelove.guo@gmail.com>
2017-02-17 00:26:09 +0800

    1px 像素实现

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <ul>
         <li class="tab-item">
           <router-link to="/goods">商品</router-link>
@@ -10,7 +10,7 @@
           <router-link to="/ratings">评价</router-link>
         </li>
         <li class="tab-item">
-          <router-link  to="/seller">店铺</router-link>
+          <router-link   to="/seller">店铺</router-link>
         </li>
       </ul>
     </div>
@@ -29,8 +29,11 @@ export default {
 </script>
 
 <style lang="less" rel="stylesheet/less">
+@import './common/less/mixin.less'; 
 #app {
   .tab {
+    // border-bottom : 1px solid rgba(7, 17, 27, 0.1);
+    .border-1px(rgba(7, 17, 27, 0.1));
     & ul {
       padding: 0;
       margin: 0;
@@ -43,8 +46,11 @@ export default {
         text-align: center;
         & > a {
           display: block;
-          text-decoration: none;
-          color: #333;
+          color: rgb(77, 85, 93);
+          font-size: 14px;
+          &.active {
+            color: rgb(240, 20, 20)
+          }
         }
       }
     }
