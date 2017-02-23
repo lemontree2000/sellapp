95d4087d9a976fef0345cd8568e74309e5eaba4f
lemontree <gonelove.guo@gmail.com>
2017-02-23 23:51:03 +0800

    shopcart updata

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -31,6 +31,57 @@
     z-index: 100;
     height: 48px;
     width: 100%;
-    background: #000; 
+    .content {
+      display: flex;
+      height: 100%;
+      background: #141d27;
+      font-size: 0;
+      .content-left {
+        flex: 1;
+        position: relative;
+        .logo-wrapper {
+          display: inline-block;
+          box-sizing: border-box;
+          position: absolute;
+          top: -10px;
+          margin: 0 12px;
+          width: 56px;
+          height: 56px;
+          padding: 6px;
+          vertical-align: top;
+          border-radius: 50%;
+          background-color: #141d27;
+          .logo {
+            width: 100%;
+            height: 100%;
+            background: #2b343c;
+            border-radius: 50%;
+            text-align: center;
+            .icon-shopping_cart {
+              font-size: 24px;
+              color: #80858a;
+              line-height: 44px;
+            }
+          }
+        }
+        .price {
+          display: inline-block;
+          font-size: 16px;
+          font-weight: 700px;
+          box-sizing: border-box;
+          padding-right: 12px;
+          vertical-align: top;
+          margin-top: 12px;
+          line-height: 24px;
+          border-right: 1px solid rgba(255,255,255,0.1);
+        }
+        .desc {
+          display: inline-block;
+        }
+      }
+      .content-right {
+        flex: 0 0 105px;
+      }
+    }
   }
 </style>
