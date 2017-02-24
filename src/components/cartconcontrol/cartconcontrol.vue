5d0e403284bd1d7fd9b23bcdc06cf5c1a6258b91
lemontree <gonelove.guo@gmail.com>
2017-02-24 22:12:34 +0800

    完成购物栏基础

---
+++ src/components/cartconcontrol/cartconcontrol.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cartconcontrol">
+    <div class="cart-decrrease" v-show="food.count>0"></div>
+    <div class="cart-count" v-show="food.count>0"></div>
+    <div class="cart-add"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    }
+  };
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  
+</style>
--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -2,17 +2,18 @@
   <div class="shopcart">
     <div class="content">
       <div class="content-left">
-        <div class="logo-wrapper">
-          <div class="logo">
-            <span class="icon-shopping_cart"></span>
+        <div class="logo-wrapper" >
+          <div class="logo" :class="{'highlight': totalCount > 0}">
+            <span class="icon-shopping_cart" :class="{'highlight': totalCount > 0}"></span>
           </div>
+          <div class="num" v-show="totalCount">{{totalCount}}</div>
         </div>
-        <div class="price">89元</div>
+        <div class="price" :class="{'highlight': totalCount > 0}">￥{{totalPrice}}</div>
         <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">
-          ￥{{minPrice}}
+        <div class="pay" :class="classPay">
+          {{payDesc}}
         </div>
       </div>
     </div>
@@ -22,6 +23,15 @@
 <script>
   export default {
     props: {
+      selectFoods: {
+        type: Array,
+        default () {
+          return [{
+            price: 22,
+            count: 1
+          }];
+        }
+      },
       deliveryPrice: {
         type: Number,
         default: 0
@@ -29,6 +39,39 @@
       minPrice: {
         type: Number,
         default: 0
+      }
+    },
+    computed: {
+      totalPrice() {
+        let total = 0;
+        this.selectFoods.forEach((food) => {
+          total += food.price * food.count;
+        });
+        return total;
+      },
+      totalCount() {
+        let count = 0;
+        this.selectFoods.forEach((food) => {
+          count += food.count;
+        });
+        return count;
+      },
+      payDesc() {
+        if (this.totalPrice === 0) {
+          return `￥${this.minPrice}元起送`;
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice;
+          return `还差￥${diff}元起送`;
+        } else {
+          return '去结算';
+        }
+      },
+      classPay() {
+        if (this.totalPrice < this.minPrice) {
+          return 'not-enough';
+        } else {
+          return 'enough';
+        }
       }
     }
   };
@@ -47,6 +90,7 @@
       height: 100%;
       background: #141d27;
       font-size: 0;
+      color: rgba(255,255,255,0.4);
       .content-left {
         flex: 1;
         position: relative;
@@ -62,16 +106,37 @@
           vertical-align: top;
           border-radius: 50%;
           background-color: #141d27;
+          .num {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 24px;
+            height: 16px;
+            line-height: 16px;
+            text-align: center;
+            border-radius: 16px;
+            font-size:  9px;
+            font-weight: 700;
+            color: #fff;
+            background:rgb(240,20,20);
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+          }
           .logo {
             width: 100%;
             height: 100%;
             background: #2b343c;
             border-radius: 50%;
             text-align: center;
+            &.highlight {
+              background: rgb(0,160,220);
+            }
             .icon-shopping_cart {
               font-size: 24px;
               color: #80858a;
               line-height: 44px;
+              &.highlight {
+                color: #fff;
+              }
             }
           }
         }
@@ -85,7 +150,9 @@
           margin-top: 12px;
           line-height: 24px;
           border-right: 1px solid rgba(255,255,255,0.1);
-          color: rgba(255,255,255,0.4);
+          &.highlight {
+            color: #fff;
+          }
         }
         .desc {
           display: inline-block;
@@ -93,13 +160,25 @@
           margin: 12px 0 0 12px;
           line-height: 24px;
           font-size: 10px;
-          color: rgba(255,255,255,0.4);
         }
       }
       .content-right {
         flex: 0 0 105px;
         width: 105px;
-        .pay
+        .pay {
+          line-height: 48px;
+          height: 48px;
+          text-align: center;
+          font-size: 12px;
+          background-color: #2b333b;
+          &.not-enough {
+            background:#2b333b;
+          }
+          &.enough {
+            background: #00b43c;
+            color: #fff;
+          }          
+        }
       }
     }
   }
