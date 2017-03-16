397cfeb8dab87e89b4a52bf0fc85fa78696b0949
lemontree <gonelove.guo@gmail.com>
2017-03-16 22:36:04 +0800

    rating

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ratingselect">
-    <div class="rating-type">
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+    <div class="rating-type border-1px">
+      <span class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count"></span>22</span>
+      <span class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">3</span></span>
+      <span class="block nagative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">4</span></span>
     </div>
     <div class="switch">
       <span class="icon-check_circle"></span>
@@ -54,6 +54,38 @@
       padding: 18px 0;
       margin: 0 18px;
       .border-1px(rgba(7,17,27,0.3));
+      font-size: 0;
+      .block {
+        display: inline-block;
+        padding: 8px 12px;
+        margin-right: 8px;
+        line-height: 16px;
+        border-radius: 1px;
+        color: rgb(77,85,93);
+        font-size: 12px;
+        &.active {
+          color: #fff;
+        }
+        .count {
+          font-size: 8px;
+          margin-left: 2px;
+        }
+        &.positive {
+          background: rgba(0,160,220,0.2);
+          &.active {
+            background: rgb(0,160,220);
+          }
+        }
+        &.negative {
+          bakcground: rgb(77,85,93,0.2);
+          &.active {
+            background: rgb(77,85,93);
+          }
+        }
+      }
+    }
+    .switch {
+      padding: 12px 18px;
     }
   }  
 </style>
