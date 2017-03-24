9b68a6bc7b9ae1b1cf9aa32e5b5598dd632a09d8
lemontree <gonelove.guo@gmail.com>
2017-03-24 23:53:10 +0800

    日期格式 5

--- src/common/js/date.js
+++ src/common/js/date.js
@@ -1,3 +1,17 @@
-export formatDate(date, fmt) {
-    
-}
+export formatDate (date, fmt) {
+  if (/(y+)/.text(fmt)) {
+    fmt = fmt.replace(RegExp.$1,(date.getFullYear() + '').substr(4-RegExp.$1.length));
+  }
+  let o = {
+    'M+': date.getMouth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getMinutes(),
+    's+': date.getSeconds()
+  };
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + '';
+      fmt = fmt.replace
+    }
+  }
+};
