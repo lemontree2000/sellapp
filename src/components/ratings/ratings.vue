67c59587bfd4e1f43ebaccc698b833af05751b12
lemontree <gonelove.guo@gmail.com>
2017-03-26 23:41:10 +0800

    ratings 第二部

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -7,23 +7,43 @@
           <div class="title">综合评分</div>
           <div class="rank">高于周边商家{{seller.rankRate}}</div>
         </div>
-        <div class="overview-right"></div>
+        <div class="overview-right">
+          <div class="score-wrapper"> 
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper"> 
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>  
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>            
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import star from '../../components/star/star';
   export default {
     props: {
       seller: {
         type: Object
       }
+    },
+    components: {
+      'star': star
     }
   };
 </script>
 
 <style lang="less" rel="stylesheet/less">
+
   .ratings {
     position: absolute;
     top: 174px;
@@ -60,6 +80,41 @@
       .overview-right {
         flex: 1;
         padding-left: 24px;
+        line-height: 18px;
+        .score-wrapper {
+          margin-bottom: 8px;
+          display: inline-block;
+          vertical-align: top;       
+          font-size: 0;
+          .title {
+            font-size: 12px;
+            color: rgb(7,17,27);
+          }
+          .star {
+            display: inline-block;
+            vertical-align: top;
+            margin: 0 12px;
+          }
+          .score {
+            line-height: 18px;
+            display: inline-block;
+            vertical-align: top;
+            font-size: 12px;  
+            color: rgb(255,153,0);          
+          }
+        }
+        .delivery-wrapper {
+          font-size: 0;
+          .title {
+            font-size: 12px;
+            color: rgb(7,17,27);
+          }
+          .delivery {
+            font-size: 12px;
+            color: rgb(147, 153, 159);
+            margin-left: 12px;
+          }
+        }
       }
     }
   }
